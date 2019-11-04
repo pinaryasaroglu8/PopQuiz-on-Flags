eb7e08c637d9740d9662c8bb16ec5ff091d646de
pinaryasaroglu8 <yasaroglu_pinar@hotmail.com>
2019-11-04 13:43:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.myapplication;
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Vibrator;
+import java.io.*;
+import java.util.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+
+    Button button1;
+    Button button2;
+    Button button3;
+
+    TextView textView;
+
+    ArrayList<Integer> flags = new ArrayList<Integer>();
+    ArrayList<Integer> random = new ArrayList<Integer>();
+    ArrayList<String> buttonTexts = new ArrayList<String>();
+
+    int a;
+
+
+    ArrayList<String> arr = new ArrayList<String>();
+
+    String mLine;
+    Object answerOnButton;
+
+    int questionNumber = 1;
+
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        image = (ImageView) findViewById(R.id.imageView2);
+
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        for (int i = 0; i < 51; i++) {
+            random.add(i);
+        }
+
+        Collections.shuffle(random);
+
+        a=random.get(0);
+        readData();
+        algorithm();
+
+
+    }
+
+    public void readData() {
+
+        for (int i = 0; i < 51; i++) {
+            flags.add(getResources().getIdentifier("flag" + i, "drawable", getPackageName()));
+
+        }
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(getAssets().open("Flag50.txt")));
+
+            while ((mLine = reader.readLine()) != null) {
+
+
+                buttonTexts.add(mLine);
+            }
+        } catch (IOException e) {
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+    }
+
+
+    public void algorithm() {
+
+
+        for (int i = 0; i < 3; i++) {
+            arr.add(buttonTexts.get(random.get(i)));
+        }
+        a=random.get(0);
+
+        image.setImageBitmap(null);
+        int b = flags.get(random.get(0));
+        image.setImageResource(flags.get(random.get(0)));//Imag
+
+        answerOnButton = arr.get(0);
+
+        Collections.shuffle(arr);
+
+        textView.setText("Question " + questionNumber);
+        questionNumber++;
+
+        button1.setText(arr.get(0));
+        button2.setText(arr.get(1));
+        button3.setText(arr.get(2));
+
+
+        buttonClick();
+
+
+    }
+
+
+    public void buttonClick() {
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (button1.getText() == answerOnButton) {
+
+                    arr.clear();
+                    random.remove(0);
+                    algorithm();
+                } else {
+                    Toast();
+                }
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (button2.getText() == answerOnButton) {
+                    arr.clear();
+                    random.remove(0);
+                    algorithm();
+
+                } else {
+                    Toast();
+                }
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (button3.getText() == answerOnButton) {
+
+                    arr.clear();
+                    random.remove(0);
+                    algorithm();
+                } else {
+                    Toast();
+                }
+            }
+        });
+
+    }
+
+
+    public void Toast() {
+        final Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vib.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            vib.vibrate(500);
+        }
+    }
+
+}
